40f09ace6bfb77d20582f726c92a951749cbd170
NakshatraAich <mounteverest247@gmail.com>
2024-06-24 12:57:43 +0530

    Added parallax to about content

--- src/app/About/page.jsx
+++ src/app/About/page.jsx
@@ -5,7 +5,8 @@ import LogoScroller from "../../components/InfiniteScroller/LogoScroller";
 import EmployeeCard from "../../components/EmployeeCard";
 import EmployeeCardII from "../../components/EmployeeCardII";
 import { down, placeholder } from "../../../public/assets";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { motion,  useScroll,  useTransform,  MotionValue, } from "framer-motion";
 
 const Leads = [
   { name: 'Alice', link: placeholder, dept: 'Member' },
@@ -35,7 +36,9 @@ const Members = [
   { name: 'Alice', link: placeholder, dept: 'Member' },
 ];
 
-
+function useParallax(value) {
+  return useTransform(value, [0, 1], ["5%", "60%"]);
+}
 
 export default function About() {
 
@@ -45,21 +48,51 @@ export default function About() {
     setIsCollapsed(!isCollapsed);
   };
 
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+  const y = useParallax(scrollYProgress);
+
     return (
-      <section className="flex flex-col items-center font-poppins sm:mt-24 mt-12  overflow-hidden">
+      <section ref={ref} className="flex flex-col items-center font-poppins sm:mt-24 mt-12  overflow-hidden">
         
-        <div className="text-5xl flex flex-col items-center  px-7 xl:px-0">
+        <motion.div 
+        initial={{y: 20,opacity:0}}
+        animate={{y: 10, opacity:1}}
+        transition={{
+          duration: 0.35,
+          stiffness: 2000,
+          dampening: 200,
+          delay: 0.25
+        }}
+          className="text-5xl flex flex-col items-center px-7 xl:px-0">
           <div className="lg:text-6xl text-5xl font-bold text-heading mt-4 text-center">
             About <span className="movis bg-gradient-to-r from-[#00A778] via-[#F3DE8A] to-[#ED6A5A] bg-clip-text text-transparent">movis.</span>
           </div>
           <div className="text-center text-sm md:text-xl text-light font-medium mt-2 max-w-[850px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </div>
-        </div>
+        </motion.div>
 
         <Image className="md:hidden mb-6 mt-4" src={down}></Image>
 
         <div className="sm:mt-24 mt-12 w-full max-w-[1150px] flex flex-col items-center gap-12 sm:gap-24 xl:px-0 sm:px-16 px-7">
+          <div className="flex sm:flex-row flex-col w-full md:gap-12 gap-8">
+            <div className="aspect-[4/5] sm:aspect-video grow rounded-lg sm:rounded-2xl bg-out basis-3/4"></div> 
+            <motion.div style={{ y }} className="flex flex-col basis-1/4 gap-1">
+              <h1 className="font-semibold text-2xl text-textAlt">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+              <h2 className="font-inter text-sm text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum.</h2>
+            </motion.div>
+          </div>
+          <div className="flex flex-col sm:flex-row-reverse w-full md:gap-12 gap-8">
+            <div className="aspect-[4/5] sm:aspect-video grow rounded-lg sm:rounded-2xl bg-out basis-3/4"></div> 
+            <motion.div style={{ y }} className="flex flex-col basis-1/4 gap-1 -mt-12">
+              <h1 className="font-semibold text-2xl text-textAlt">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+              <h2 className="font-inter text-sm text-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum.</h2>
+            </motion.div>
+          </div>
+        </div>
+
+        <div className="lg:hidden sm:mt-24 mt-12 w-full max-w-[1150px] flex flex-col items-center gap-12 sm:gap-24 xl:px-0 sm:px-16 px-7">
           <div className="flex sm:flex-row flex-col w-full md:gap-12 gap-8">
             <div className="aspect-[4/5] sm:aspect-video grow rounded-lg sm:rounded-2xl bg-out basis-3/4"></div> 
             <div className="flex flex-col basis-1/4 gap-1">
